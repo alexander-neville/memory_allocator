203e688b0fa967de88a933f83afefd2211b053f7
Alexander Neville <alexander.neville@icloud.com>
2021-12-15 18:42:04 +0000

    syscall memory allocator is working

--- main/test_arr_alloc.c
+++ main/test_arr_alloc.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "../inc/arr_alloc.h"
@@ -6,13 +5,6 @@
 int main() {
     int * test1 = (int *) arr_malloc(sizeof(int) * 20);
     if (!test1) exit(0);
-    // printf("%p\n", test1);
-    // for (int i = 0; i < 5; i++) {
-    //     test1[i]= i;
-    // }
-    // for (int i = 0; i < 5; i++) {
-    //     printf("%d\n", test1[i]);
-    // }
     int * test2 = (int *) arr_malloc(sizeof(int) * 5);
     if (!test2) exit(0);
     arr_free(test1);
@@ -21,13 +13,15 @@ int main() {
     int * test4 = (int *) arr_malloc(sizeof(int) * 5);
     if (!test4) exit(0);
     print_mem_layout((slot_header *) (((char *) test1) - HEADER_SIZE));
-    // int * test3 = (int *) arr_malloc(sizeof(int) * 2);
-    // if (!test3)
-    //     exit(0);
-    // printf("%p\n", test3);
-    // int * test4 = (int *) arr_malloc(sizeof(int) * 2);
-    // if (!test4)
-    //     exit(0);
-    // printf("%p\n", test4);
+    arr_free(test4);
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    print_mem_layout((slot_header *) (((char *) test1) - HEADER_SIZE));
+    printf("\n");
+    void * valid = arr_malloc(1000);
+    if (!valid)
+        printf("invalid request returns NULL\n");
     return 0;
 }
